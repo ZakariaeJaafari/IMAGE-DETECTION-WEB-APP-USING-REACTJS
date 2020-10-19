3b1928b9eb930e7303363fe56f98420d839a0922
zackinsea <zjaafari@insea.ac.ma>
2020-10-20 00:21:28 +0100

    Updates

---
+++ precache-manifest.db1ab3a314e066c3df1f40b09f1231d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bba1e62cd529a507581057748a2fd69",
+    "url": "/ZakariaeJaafari/IMAGE-DETECTION-WEB-APP-USING-REACTJS/index.html"
+  },
+  {
+    "revision": "dca53c8cae0d4e78de8a",
+    "url": "/ZakariaeJaafari/IMAGE-DETECTION-WEB-APP-USING-REACTJS/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bdb980aa3522676206c0",
+    "url": "/ZakariaeJaafari/IMAGE-DETECTION-WEB-APP-USING-REACTJS/static/css/main.f0bd9fb4.chunk.css"
+  },
+  {
+    "revision": "dca53c8cae0d4e78de8a",
+    "url": "/ZakariaeJaafari/IMAGE-DETECTION-WEB-APP-USING-REACTJS/static/js/2.6a572f96.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ZakariaeJaafari/IMAGE-DETECTION-WEB-APP-USING-REACTJS/static/js/2.6a572f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb980aa3522676206c0",
+    "url": "/ZakariaeJaafari/IMAGE-DETECTION-WEB-APP-USING-REACTJS/static/js/main.43255459.chunk.js"
+  },
+  {
+    "revision": "d462b09a81adec3bfff5",
+    "url": "/ZakariaeJaafari/IMAGE-DETECTION-WEB-APP-USING-REACTJS/static/js/runtime-main.690024ce.js"
+  }
+]);
